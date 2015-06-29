7da950a2f6d11a49f37fbf10d534f10f36c2ef90
Rémi Chaignon <remi.chaignon@gmail.com>
2015-06-28 22:50:29 -0600

    Stacking rows of boards

--- app/pods/board/model.js
+++ app/pods/board/model.js
@@ -8,6 +8,46 @@ export default DS.Model.extend({
   playerBlack: Ember.computed.alias("players.lastObject"),
 
   spots: DS.hasMany("spot", { async: true }),
+  spotsOnRow1: function () {
+    return this.get("spots").filter(function (spot) {
+      return (spot.get("name") || [])[1] === "1";
+    });
+  }.property("spots.@each.name"),
+  spotsOnRow2: function () {
+    return this.get("spots").filter(function (spot) {
+      return (spot.get("name") || [])[1] === "2";
+    });
+  }.property("spots.@each.name"),
+  spotsOnRow3: function () {
+    return this.get("spots").filter(function (spot) {
+      return (spot.get("name") || [])[1] === "3";
+    });
+  }.property("spots.@each.name"),
+  spotsOnRow4: function () {
+    return this.get("spots").filter(function (spot) {
+      return (spot.get("name") || [])[1] === "4";
+    });
+  }.property("spots.@each.name"),
+  spotsOnRow5: function () {
+    return this.get("spots").filter(function (spot) {
+      return (spot.get("name") || [])[1] === "5";
+    });
+  }.property("spots.@each.name"),
+  spotsOnRow6: function () {
+    return this.get("spots").filter(function (spot) {
+      return (spot.get("name") || [])[1] === "6";
+    });
+  }.property("spots.@each.name"),
+  spotsOnRow7: function () {
+    return this.get("spots").filter(function (spot) {
+      return (spot.get("name") || [])[1] === "7";
+    });
+  }.property("spots.@each.name"),
+  spotsOnRow8: function () {
+    return this.get("spots").filter(function (spot) {
+      return (spot.get("name") || [])[1] === "8";
+    });
+  }.property("spots.@each.name"),
 
   name: DS.attr("string"),
 
