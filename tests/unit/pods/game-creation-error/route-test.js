34d1fc29fc02c9e407c055eccb49d42340f0972f
Rémi Chaignon <remi.chaignon@gmail.com>
2015-08-02 22:41:17 -0600

    Missing game creation error route

---
+++ tests/unit/pods/game-creation-error/route-test.js
@@ -0,0 +1,10 @@
+import { moduleFor, test } from "ember-qunit";
+
+moduleFor("route:game-creation-error", "Unit | Route | game creation error");
+
+test("it exists", function(assert) {
+  assert.expect(1);
+
+  var route = this.subject();
+  assert.ok(route);
+});
