0c07b4971f7d113f04268257447974f4830c1e2e
Rémi Chaignon <remi.chaignon@gmail.com>
2015-09-12 10:47:27 -0600

    Test session service initializer

--- tests/unit/initializers/session-service-test.js
+++ tests/unit/initializers/session-service-test.js
@@ -5,21 +5,26 @@ import { expect, module, test } from "qunit";
 var container, application;
 
 module("Unit | Initializer | session service", {
-  // beforeEach: function () {
-  //   Ember.run(function () {
-  //     application = Ember.Application.create();
-  //     container = application.__container__;
-  //     application.deferReadiness();
-  //   });
-  // }
+  beforeEach: function () {
+    Ember.run(function () {
+      application = Ember.Application.create();
+      container = application.__container__;
+      application.deferReadiness();
+    });
+  }
 });
 
 test("inject", function (assert) {
-  assert.expect(0);//3);
+  assert.expect(3);
 
-  // initialize(container, application);
-  //
-  // assert.ok(container.lookup("component:test").session, "Session service is injected onto components.");
-  // assert.ok(container.lookup("controller:test").session, "Session service is injected onto controller.");
-  // assert.ok(container.lookup("route:test").session, "Session service is injected onto routes.");
+  application.register("service:session", Ember.Object);
+  application.register("component:test", Ember.Object);
+  application.register("controller:test", Ember.Object);
+  application.register("route:test", Ember.Object);
+
+  initialize(container, application);
+
+  assert.ok(container.lookup("component:test").session, "Session service is injected onto components.");
+  assert.ok(container.lookup("controller:test").session, "Session service is injected onto controller.");
+  assert.ok(container.lookup("route:test").session, "Session service is injected onto routes.");
 });
